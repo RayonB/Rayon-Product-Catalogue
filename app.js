7716f2c3e902b659fc0682abae0305ba7bdcb0fc
RayonB <rinevarayon@gmail.com>
2024-02-15 18:27:34 +0800

    Cay

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
   
 ProductCardsContainer.classList.add("card-group"); // Adding card-group class to productCardsContainer
 
-        data.products.forEach(Product => {
-          const card = document.createElement("div");
+    data.products.forEach(Product => {
+        const card = document.createElement("div");
+            <img src="${product.product_image}" class="card-img-top" alt="${Product.Product_Name}">
+            <div class="card-body">
+            <h5 class="card-title">${Product.Product_Name}</h5>
+            <p class="card-text">${Product.Product_Description}</p>
   
