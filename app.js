6a05647cee92f643e9a693f8c0a45e79be114e9b
RayonB <rinevarayon@gmail.com>
2024-02-15 20:11:17 +0800

    hapit

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
         const ProductListItemsContainer = document.getElementById("cart-items");
         const ProductList = {}; // Object to store cart items and their quantities
   
-ProductCardsContainer.classList.add("card-group"); // Adding card-group class to productCardsContainer
+ProductCardsContainer.classList.add("card-group"); // Adding card-group class to ProductCardsContainer
 
 data.products.forEach(Product => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-      <img src="${Product.Product_Image}" class="card-img-top" alt="${Product.Product_Name}">
-      <div class="card-body">
-        <h5 class="card-title">${Product.Product_Name}</h5>
-        <p class="card-text">${Product.Product_Description}</p>
-        <p class="card-text">Price: $${Product.Product_Price.toFixed(2)}</p>
-        <p class="card-text"><small class="text-body-secondary">Date Added: ${Product.Product_Date_Added}</small></p>
-        <p class="card-text"><small class="text-body-secondary">Expiration Date: ${Product.Product_Expiration_Date}</small></p>
-        <button class="btn btn-primary add-to-cart" data-product="${Product.Product_Name}">Add to Cart</button>
-      </div>
-    `;
+    <img src="${Product.Product_Image}" class="card-img-top" alt="${Product.Product_Name}">
+    <div class="card-body">
+      <h5 class="card-title">${Product.Product_Name}</h5>
+      <p class="card-text">${Product.Product_Description}</p>
+      <p class="card-text">Price: $${Product.Product_Price.toFixed(2)}</p>
+      <p class="card-text"><small class="text-body-secondary">Date Added: ${Product.Product_Date_Added}</small></p>
+      <p class="card-text"><small class="text-body-secondary">Expiration Date: ${Product.Product_Expiration_Date}</small></p>
+      <button class="btn btn-primary add-to-cart" data-product="${Product.Product_Name}">Add to Cart</button>
+    </div>
+  `;
+
     ProductCardsContainer.appendChild(card);
   });
 
