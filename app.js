2fa76456fa7c96b83eec6b693846ed99202ed934
RayonB <rinevarayon@gmail.com>
2024-02-15 17:55:22 +0800

    ediiirrrttt

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("data.json")
       .then(response => response.json())
       .then(data => {
-        const productCardsContainer = document.getElementById("product-cards");
-        const cartItemsContainer = document.getElementById("cart-items");
+        const ProductCardsContainer = document.getElementById("product-cards");
+        const CartItemsContainer = document.getElementById("cart-items");
         const cart = {}; // Object to store cart items and their quantities
   
-        productCardsContainer.classList.add("card-group"); // Adding card-group class to productCardsContainer
+        ProductCardsContainer.classList.add("card-group"); // Adding card-group class to productCardsContainer
   
-        data.products.forEach(product => {
+        data.products.forEach(Product => {
           const card = document.createElement("div");
         
